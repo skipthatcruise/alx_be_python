479e325919e64f33bc6e8e82bdece36d1c7ab01d
fiyinfoluwa <fiyinfoluwaoyekan@gmail.com>
2024-12-16 22:54:02 +0100

    Python code for basic operations was created

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+thesum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+print("Addition of {} and {} is {}".format(number1, number2, thesum))
+print("Subtraction of {} and {} is {}".format(number1, number2, difference))
+print("Multiplication of {} and {} is {}".format(number1, number2, product))
