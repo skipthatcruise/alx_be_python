48be05e10dd828bf900b6857760170b98044adb3
fiyinfoluwa <fiyinfoluwaoyekan@gmail.com>
2024-12-17 00:18:50 +0100

    A python script to calculate the projected savings after one year

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,9 @@
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
+interest_rate = 0.05
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * interest_rate)
+print("Your monthly savings are {}",format(monthly_savings))
+print("Projected savings after one year, with interest, is: {} ".format(projected_savings))
+
+
