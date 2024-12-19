e4f09d5536454a8521913e1d5d4a9fb806037536
fiyinfoluwa <fiyinfoluwaoyekan@gmail.com>
2024-12-19 19:57:26 +0100

    edited

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -3,6 +3,6 @@ rows = pattern_size + 1
 i = 1
 while i < rows:
     for pattern in range(1, 2):
-        print("*" * pattern_size, end = "")
+        print("*" * pattern_size, end="")
         print()
         i+=1
