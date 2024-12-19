f41b7fac0bcccd0aef6d0792f025c5e17643121b
fiyinfoluwa <fiyinfoluwaoyekan@gmail.com>
2024-12-19 19:55:25 +0100

    edited

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -2,7 +2,7 @@ pattern_size = int(input("Enter the size of the pattern: "))
 rows = pattern_size + 1
 i = 1
 while i < rows:
-    for pattern in range(1,2):
-        print("*" * pattern_size, end = " ")
+    for pattern in range(1, 2):
+        print("*" * pattern_size, end = "")
         print()
         i+=1
