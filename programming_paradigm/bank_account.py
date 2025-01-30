4223f9a8d90ab4714903e7ccdca1b8756fcc7f6d
fiyinfoluwa <fiyinfoluwaoyekan@gmail.com>
2025-01-30 12:17:19 +0100

    The bank account script is complete

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,24 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+        # self.initial_balance = initial_balance
+        # initial_balance = 0
+
+    def deposit(self, amount):
+        # print(f"You have deposited ${amount}. Current balance is: ${amount + self.account_balance}")
+        return amount + self.account_balance
+
+    def withdraw(self, amount):
+        if amount < self.account_balance:
+            self.account_balance -= amount
+            # print(f"You have withdrawn ${amount}. Current balance is: ${self.account_balance - amount}")
+            return True
+
+        else:
+            # print("Insufficient funds or invalid amount.")
+            return False
+
+    def display_balance(self):
+        print(f"Your current balance is ${self.account_balance}")
+
+
