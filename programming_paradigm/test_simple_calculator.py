ea667684b74b2f9f2d153bfa3230c427331d3788
fiyinfoluwa <fiyinfoluwaoyekan@gmail.com>
2025-01-31 05:17:00 +0100

    Test addition edited

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,12 +6,12 @@ class TestCalculator(unittest.TestCase):
         """Set up the SimpleCalculator instance before each test."""
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2,3),5)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(0,0),0)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5,3),2)
         self.assertEqual(self.calc.subtract(2,0),2)
         self.assertEqual(self.calc.subtract(5,6),-1)
